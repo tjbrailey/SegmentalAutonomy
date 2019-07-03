4e89a0613e77ad5c9cc5085260cf01029f101c46
tjbrailey <tbrailey@ucsd.edu>
2019-07-03 12:36:49 -0700

    code now installs current datasets.

--- code/senior_thesis_code.R
+++ code/senior_thesis_code.R
@@ -1,9 +1,37 @@
+### Thomas Brailey Senior Thesis Code ### 
+      ### POLI 191A // POLI 191B ###
 
+# Set working directory
+setwd(paste0(here::here(), '/data/'))
+wd <- paste0(here::here(), '/data/')
 getwd()
 
-idc <- rio::import(file = paste0(here::here(), '/data/IDC_country-year_v1_0.RData'))
-idc <- rio::import(file = paste0(here::here(), '/data/PSED_agreement.xlsx'))
+# Load for piping function
+library(magrittr)
+library(ggplot2)
+library(ggmap)
 
-dplyr::as_data_frame(idc)
-summary(idc)
-idc 
+# Parse out sheets from PSED excel workbook
+files <- list.files(wd, "PSED_agreement.xlsx")
+files <- files[]
+
+read_excel_allsheets <- function(filename) { 
+  sheets <- readxl::excel_sheets(filename) 
+  x <- lapply(sheets, function(x) readxl::read_excel(filename, sheet = x)) 
+  names(x) <- sheets 
+  x 
+} 
+
+out <- lapply(files, read_excel_allsheets)
+basename(files)
+
+# Install datasets
+psed_prom <- out[[1]]$PSED_agreement_promises
+psed_prac <- out[[1]]$PSED_agreement_practices
+idc <- rio::import('IDC_country-year_v1_0.RData')
+impact <- rio::import('c_656154-l_1-k_impact--version2.0.csv') # IMPACT data previously in .xls. Updated to .csv.
+
+# Clean workspace
+rm(out, files, read_excel_allsheets)
+
+# Data analysis
