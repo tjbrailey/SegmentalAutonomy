963caab7f3f1c3745f1c1c5e794b591d10338810
tjbrailey <tbrailey@ucsd.edu>
2019-09-10 05:27:02 -0700

    9/10/19

--- code/senior_thesis_code.R
+++ code/senior_thesis_code.R
@@ -77,12 +77,41 @@ tb$cowc <- countrycode::countrycode(tb$country, 'country.name', 'cowc')
 tb$cown <- countrycode::countrycode(tb$country, 'country.name', 'cown')
 
 tb <- tb %>%
-  dplyr::select(country, cowc, cown, year, gwno, ifs, dplyr::everything())
+  dplyr::select(country, 
+                cowc, 
+                cown, 
+                year, 
+                gwno, 
+                ifs, 
+                mveto,
+                gcman,
+                gcimp,
+                auton,
+                jrevman,
+                relconstd,
+                relconstp,
+                milleg,
+                partynoethnic,
+                jtenure,
+                jconst,
+                gcseats1,
+                gcseats2,
+                gcseats3,
+                unity,
+                resman,
+                resseats,
+                resseats2,
+                resseatsimp,
+                miman,
+                subtax,
+                subed,
+                subpolice,
+                state
+                )
 
 # Prepare other data
 dtd_psp_sub <- dtd %>%
-  dplyr::select(Refno,
-                Nation,
+  dplyr::select(Nation,
                 CCode,
                 Year,
                 Natcode,
@@ -100,23 +129,51 @@ dtd_psp_sub <- dtd %>%
                 prDPI,
                 roseprop,
                 xrcomp
-                )
-  
+                ) %>%
+  dplyr::rename(country = Nation,
+                year = Year)
+dtd_psp_sub$country <- countrycode::countrycode(dtd_psp_sub$country, 'country.name', 'country.name')
+dtd_psp_sub$cowc <- countrycode::countrycode(dtd_psp_sub$country, 'country.name', 'cowc')
+dtd_psp_sub$cown <- countrycode::countrycode(dtd_psp_sub$country, 'country.name', 'cown')  
+
 pax_psp_sub <- pax %>%
-  dplyr::select(PpsAut,
+  dplyr::select(Con,
+                Dat,
+                Agt,
+                PpsAut,
                 TpsAut,
                 PpsEx,
                 CenBan,
-                
                 PpsOro,
                 PpsOthPr,
                 StRef
-                )
+                ) %>%
+  dplyr::rename(country = Con,
+                year = Dat) %>%
+  dplyr::mutate(year = as.numeric(stringr::str_sub(year, start= -4))) %>%
+  dplyr::group_by(PpsAut,
+                  TpsAut,
+                  PpsEx,
+                  CenBan,
+                  PpsOro,
+                  PpsOthPr,
+                  StRef, 
+                  country, year) %>%
+  dplyr::summarise(Agt = paste(Agt, collapse=", ")) %>% 
+  dplyr::ungroup() %>%
+  dplyr::mutate(country = sub("\\/.*", "", country)) %>%
+  dplyr::select(-Agt)
+pax_psp_sub$country <- countrycode::countrycode(pax_psp_sub$country, 'country.name', 'country.name')
+pax_psp_sub$cown <- countrycode::countrycode(pax_psp_sub$country, 'country.name', 'cown')
+pax_psp_sub$cowc <- countrycode::countrycode(pax_psp_sub$country, 'country.name', 'cowc')
 
 qog_ts_psp_sub <- qog_ts %>%
-  dplyr::select(fe_etfra,
+  dplyr::select(ccode, 
+                cname, 
+                year, 
+                ccodealp,
+                fe_etfra,
                 iaep_ebbp,
-                dpi_pr,
                 gtm_unit,
                 ccp_hr,
                 ffp_hr,
@@ -142,34 +199,94 @@ qog_ts_psp_sub <- qog_ts %>%
                 iaep_lego,
                 iaep_lrit,
                 wbgi_pve
-  )
+  ) %>%
+  dplyr::rename(country = cname)
+qog_ts_psp_sub$country[qog_ts_psp_sub$country == "Micronesia"] <- "Federated States of Micronesia"
+qog_ts_psp_sub$country[qog_ts_psp_sub$country == "Serbia and Montenegro"] <- "Montenegro"
+qog_ts_psp_sub$country <- countrycode::countrycode(qog_ts_psp_sub$country, 'country.name', 'country.name')
+qog_ts_psp_sub$cown <- countrycode::countrycode(qog_ts_psp_sub$country, 'country.name', 'cown')
+qog_ts_psp_sub$cowc <- countrycode::countrycode(qog_ts_psp_sub$country, 'country.name', 'cowc')
 
 dpi_psp_sub <- dpi %>%
-  dplyr::select(auton,
+  dplyr::select(countryname, 
+                ifs, 
+                year,
+                system,
+                auton,
                 pr,
                 sensys,
                 eiec
-                )
+                ) %>%
+  dplyr::rename(country = countryname) %>%
+  dplyr::mutate(year = as.numeric(year))
+dpi_psp_sub$country[dpi_psp_sub$country == "Cent. Af. Rep."] <- "Central African Republic"
+dpi_psp_sub$country[dpi_psp_sub$country == "Dom. Rep."] <- "Dominican Republic"
+dpi_psp_sub$country[dpi_psp_sub$country == "GDR"] <- "German Democratic Republic"
+dpi_psp_sub$country[dpi_psp_sub$country == "PRC"] <- "China"
+dpi_psp_sub$country[dpi_psp_sub$country == "PRK"] <- "North Korea"
+dpi_psp_sub$country[dpi_psp_sub$country == "ROK"] <- "South Korea"
+dpi_psp_sub$country[dpi_psp_sub$country == "S. Africa"] <- "South Africa"
+dpi_psp_sub$country <- countrycode::countrycode(dpi_psp_sub$country, 'country.name', 'country.name')
+dpi_psp_sub$cowc <- countrycode::countrycode(dpi_psp_sub$country, 'country.name', 'cowc')
+dpi_psp_sub$cown <- countrycode::countrycode(dpi_psp_sub$country, 'country.name', 'cown')
 
 impact_psp_sub <- impact %>%
-  dplyr::select(TERRPACT,
+  dplyr::select(`Country name`,
+                `PA name`,
+                `PA year`, 
+                TERRPACT,
                 POLPACT
-                )
+                ) %>%
+  dplyr::group_by(`Country name`, `PA year`, TERRPACT, POLPACT) %>%
+  dplyr::summarise(`PA name` = paste(`PA name`, collapse=", ")) %>%
+  dplyr::rename(country = `Country name`,
+                pa_name = `PA name`,
+                year = `PA year`)
+impact_psp_sub$country[impact_psp_sub$country == "Israel (Palestine)"] <- "Israel"
+impact_psp_sub$country[impact_psp_sub$country == "UK (Northern Ireland)"] <- "United Kingdom"
+impact_psp_sub$country[impact_psp_sub$country == "Yugoslavia (Slovenia)"] <- "Yugoslavia"
+impact_psp_sub$country <- countrycode::countrycode(impact_psp_sub$country, 'country.name', 'country.name')
+impact_psp_sub$cowc <- countrycode::countrycode(impact_psp_sub$country, 'country.name', 'cowc')
+impact_psp_sub$cown <- countrycode::countrycode(impact_psp_sub$country, 'country.name', 'cown')
 
 psed <- dplyr::left_join(psed_prac, psed_prom)
 psed_psp_sub <- psed %>% 
-  dplyr::select(tps_autonomy,
+  dplyr::select(location,
+                GWNo,
+                PostStartDate,
+                tps_autonomy,
                 other_proprep,
                 pps_cabinet,
                 pps_sencabinet,
                 pps_nsencabinet
-  )
+  ) %>%
+  dplyr::group_by(location, PostStartDate) %>%
+  dplyr::distinct() %>%
+  dplyr::rename(country = location,
+                year = PostStartDate) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(year = as.numeric(stringr::str_extract(year, "^.{4}")))
+psed_psp_sub$country <- countrycode::countrycode(psed_psp_sub$country, 'country.name', 'country.name')
+psed_psp_sub$cowc <- countrycode::countrycode(psed_psp_sub$country, 'country.name', 'cowc')
+psed_psp_sub$cown <- countrycode::countrycode(psed_psp_sub$country, 'country.name', 'cown')  
+
 
 # Join data
 
+tb_1 <- dplyr::left_join(tb, dtd_psp_sub)
+tb_2 <- dplyr::left_join(tb_1, psed_psp_sub)
+tb_3 <- dplyr::left_join(tb_2, impact_psp_sub)
+tb_4 <- dplyr::left_join(tb_3, qog_ts_psp_sub)
+tb_5 <- dplyr::left_join(tb_4, pax_psp_sub)
+tb_6 <- dplyr::left_join(tb_5, dpi_psp_sub)
+
+DataExplorer::plot_missing(tb_5)
+Amelia::missmap(tb_6)
+
+
 ### Clean
 
-
+t <- dplyr::select(tb_6, country, year, Coalition, coalition2, coalitiongov, gcman, gcimp, PpsEx)
 
 ### Data analysis
 
