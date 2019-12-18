3dc9cf556cd6874b2a3d13433a11d9108422e3f5
tjbrailey <tbrailey@ucsd.edu>
2019-12-18 04:46:40 -0800

    big push

--- R/lm.R
+++ R/lm.R
@@ -7,7 +7,7 @@
 
 # Input Variables
 # - dv - Dependent Variable
-# - iv - Dependent Variable
+# - iv - Independent Variable
 
 psp_lm <- function(dv, iv){
   
@@ -16,42 +16,46 @@ psp_lm <- function(dv, iv){
   library(ggplot2)
   
   # Load data
-  psp <- rio::import(paste0(here::here(), "/data/tjbrailey_psp_clean.csv"))
+  psp <- dplyr::as_data_frame(rio::import(paste0(here::here(), "/data/tjbrailey_psp_clean.csv")))
   psp <- psp[,-1]
   
   # Subset by ethnic fractionalization
+  #psp <- psp %>%
+   # dplyr::group_by(country) %>%
+    #dplyr::mutate(no_aut = sum(idc_auton, na.rm = TRUE)) %>%
+    #dplyr::filter(sum(no_aut) != 0)
   psp <- psp %>%
-    dplyr::filter(qog_al_ethnic > median(psp$qog_al_ethnic, na.rm = TRUE))
+    dplyr::group_by(country) %>%
+    dplyr::filter(qog_fe_etfra > median(psp$qog_fe_etfra, na.rm = TRUE))
   
   
-  lm1 <- estimatr::lm_robust(get(dv) ~ get(iv) + as.factor(country), 
+  lm1 <- estimatr::lm_robust(!!sym(dv) ~ !!sym(iv) + as.factor(country), 
                              data = psp, se_type = "CR2", clusters = country)
-  lm2 <- estimatr::lm_robust(get(dv) ~ get(iv) + as.factor(country) + as.factor(year), 
+  
+  lm5 <- estimatr::lm_robust(!!sym(dv) ~ !!sym(iv) + as.factor(country) + as.factor(year) + qog_wbgi_pve + qog_wdi_gini + qog_gle_pop + polity4_polity_score, 
                              data = psp, se_type = "CR2", clusters = country)
-  lm3 <- estimatr::lm_robust(get(dv) ~ get(iv) + as.factor(country) + as.factor(year) + qog_wbgi_pve, 
-                             data = psp, se_type = "CR2", clusters = country)
-  lm4 <- estimatr::lm_robust(get(dv) ~ get(iv) + as.factor(country) + as.factor(year) + qog_wbgi_pve + qog_wdi_gini, 
-                             data = psp, se_type = "CR2", clusters = country)
-  lm5 <- estimatr::lm_robust(get(dv) ~ get(iv) + as.factor(country) + as.factor(year) + qog_wbgi_pve + qog_wdi_gini + qog_gle_pop, 
-                             data = psp, se_type = "CR2", clusters = country)
-  lm6 <- estimatr::lm_robust(get(dv) ~ (get(iv) * tb_other_provis) + as.factor(country) + as.factor(year) + qog_wbgi_pve + qog_wdi_gini + qog_gle_pop, 
+  
+  lm6 <- estimatr::lm_robust(!!sym(dv) ~ (!!sym(iv) * tb_other_provis) + as.factor(country) + as.factor(year) + qog_wbgi_pve + qog_wdi_gini + qog_gle_pop + polity4_polity_score, 
                              data = psp, se_type = "CR2", clusters = country)
   
   
   lm1_tex <- texreg::texreg(lm1, omit.coef = "as.factor", include.ci = FALSE, custom.gof.rows = list(`Fixed effects` = ("Y")))
-  lm2_tex <- texreg::texreg(lm2, omit.coef = "as.factor", include.ci = FALSE, custom.gof.rows = list(`Fixed effects` = ("Y")))
-  lm3_tex <- texreg::texreg(lm3, omit.coef = "as.factor", include.ci = FALSE, custom.gof.rows = list(`Fixed effects` = ("Y")))
-  lm4_tex <- texreg::texreg(lm4, omit.coef = "as.factor", include.ci = FALSE, custom.gof.rows = list(`Fixed effects` = ("Y")))
+  #lm2_tex <- texreg::texreg(lm2, omit.coef = "as.factor", include.ci = FALSE, custom.gof.rows = list(`Fixed effects` = ("Y")))
+  #lm3_tex <- texreg::texreg(lm3, omit.coef = "as.factor", include.ci = FALSE, custom.gof.rows = list(`Fixed effects` = ("Y")))
+  #lm4_tex <- texreg::texreg(lm4, omit.coef = "as.factor", include.ci = FALSE, custom.gof.rows = list(`Fixed effects` = ("Y")))
   lm5_tex <- texreg::texreg(lm5, omit.coef = "as.factor", include.ci = FALSE, custom.gof.rows = list(`Fixed effects` = ("Y")))
   lm6_tex <- texreg::texreg(lm6, omit.coef = "as.factor", include.ci = FALSE, custom.gof.rows = list(`Fixed effects` = ("Y")))
   
   
-  lm_full_tex <- texreg::texreg(list(lm1, lm2, lm3, lm4, lm5, lm6), 
+  lm_full_tex <- texreg::texreg(list(lm1, #lm2, lm3, lm4, 
+                                     lm5, lm6), 
                                 mfrow = TRUE, 
                                 omit.coef = "as.factor", 
                                 include.ci = FALSE, 
-                                custom.gof.rows = list(`Country fixed effects` = c("Y", "Y", "Y", "Y", "Y", "Y"),
-                                                        `Year fixed effects` = c("N", "Y", "Y", "Y", "Y", "Y")))
+                                custom.gof.rows = list(`Country fixed effects` = c("Y", #"Y", "Y", "Y", 
+                                                                                   "Y", "Y"),
+                                                        `Year fixed effects` = c("N", #"Y", "Y", "Y", 
+                                                                                 "Y", "Y")))
   
   lm_full_tex
   
